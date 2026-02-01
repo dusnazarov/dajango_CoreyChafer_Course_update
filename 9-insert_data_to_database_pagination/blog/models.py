a41ba089176bcae6cd47bf4fe33a5462aebf82a7
Elyor <dusnazarov.ed@gmail.com>
2026-02-01 22:37:53 +0500

    pagination page is commited

---
+++ 9-insert_data_to_database_pagination/blog/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse     
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):        
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+
+
+"""  
+python manage.py sh
+from django.contrib.auth.models import User
+from blog.models import Post
+
+user=User.objects.first()
+user.save() 
+post=Post(title="my first post", content="This is the content of my first post",author=user)
+post.save()
+post.author.username
+post.author.email
+posts_by_user = user.post_set.all()
+created_post = user.post_set.create(title="my second post",content="second content") 
+
+"""
+
+
+""" 
+import json
+from blog.models import Post
+>>> with open('post.json') as f:
+>>>    post_json = json.load(f)
+>>> for post in post_json:
+>>>    Post(title=post['title'], content=post['content'], date_posted=post['date_posted'], author_id=post['user_id']).save()
+  """
